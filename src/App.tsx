54eb0661ad125da66cc29189ee52916c6ab57d56
YeKate <kate.yemelyanenka@gmail.com>
2024-05-20 17:53:44 -0700

    feat: added pages to app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import ProviderPage from './pages/ProviderPage';
+import ClientPage from './pages/ClientPage';
 import Login from './components/Auth/Login.tsx';
 import LayoutWrapper from './components/ui/LayoutWrapper.tsx';
 import './App.css';
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Providers from './components/AppProviders.tsx';
@@ -13,6 +16,8 @@ const App: React.FC = () => {
       <LayoutWrapper>
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/provider" element={<ProviderPage />} />
+          <Route path="/client" element={<ClientPage />} />
           <Route path="/login" element={<Login />} />
         </Routes>
         <ToastContainer autoClose={500} />
