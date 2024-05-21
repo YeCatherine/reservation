8291080f0065ab17ac30660d8f61bb6583f8af9e
YeKate <kate.yemelyanenka@gmail.com>
2024-05-20 17:01:04 -0700

    feat: auth context

--- src/components/AppProviders.tsx
+++ src/components/AppProviders.tsx
@@ -1,13 +1,20 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './Auth/context/AuthContext.tsx';
 import ErrorBoundary from './ErrorHandler.tsx';
 import 'react-toastify/dist/ReactToastify.css';
 import '../App.css';
+
+const ContextProviders = ({ children }) => (
+  <AuthProvider>{children}</AuthProvider>
+);
 
 export default function AppProviders({ children }) {
   // Gather all providers in one place
   return (
     <ErrorBoundary>
-      <Router>{children}</Router>
+      <Router>
+        <ContextProviders>{children}</ContextProviders>
+      </Router>
     </ErrorBoundary>
   );
 }
---
+++ src/components/Auth/context/AuthContext.tsx
@@ -0,0 +1,78 @@
+import React, { createContext, ReactNode, useContext, useState } from 'react';
+import axios from 'axios';
+interface User {
+  id: string;
+  name: string;
+  role: string;
+}
+interface AuthContextProps {
+  user: User;
+  userType: string;
+  login: (username: string, password: string) => Promise<void>;
+  register: (
+    username: string,
+    password: string,
+    userType: string
+  ) => Promise<void>;
+  logout: () => void;
+}
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  // @todo remove that hardcoded user.
+  const [user, setUser] = useState({
+    id: 'provider1',
+    name: 'provider1',
+    password: 'provider1',
+    role: 'provider',
+  });
+  const [userType, setUserType] = useState('');
+
+  const login = async (username: string, password: string) => {
+    const response = await axios.post('/api/auth/login', {
+      name: username,
+      password,
+    });
+
+    setUser(response.data.user);
+    setUserType(response.data.userType);
+  };
+
+  const register = async (
+    username: string,
+    password: string,
+    userType: string
+  ) => {
+    const response = await axios.post('/api/register', {
+      username,
+      password,
+      userType,
+    });
+    setUser(response.data.user);
+    setUserType(response.data.userType);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setUserType('');
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, userType, login, register, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
